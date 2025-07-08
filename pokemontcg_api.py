0f77442e82be898506c70f64f8a8b7155a737699
yb04 <yahavbar@gmail.com>
2025-07-08 23:28:45 +0200

    added option to search cards by PokedexNumber

--- pokemontcg_api.py
+++ pokemontcg_api.py
@@ -15,43 +15,56 @@ headers = {
 }
 
 
-def get_cards(*pokemon) -> pd.DataFrame:
+def get_cards(*pokemon: str | int) -> pd.DataFrame:
     """
     Get a DataFrame including a list of all the given Pokemon.
-    :param pokemon: one or more desired Pokemon to list.
+    :param pokemon: one or more names or pokedex numbers of  desired Pokemon to list.
     :return: DataFrame including the desired List.
     """
+    strPokes = []
+    intPokes = []
+    for poke in pokemon:
+        (strPokes if isinstance(poke, str) else intPokes).append(str(poke))
+    parts = []
+    if strPokes:
+        parts.append("(name:" + " OR name:".join(strPokes) + ")")
+    if intPokes:
+        parts.append("(nationalPokedexNumbers:" + " OR nationalPokedexNumbers:".join(map(str, intPokes)) + ")")
+
+    q = " OR ".join(parts)
+
     params = {
-        "q": "(name:" + " OR name:".join(pokemon) + ")"
+        "q": q,
+        "orderBy": "-set.releaseDate"
     }
 
-    response = requests.get(BASE_URL, headers=headers, params=params)
+    try:
+        response = requests.get(BASE_URL, headers=headers, params=params, timeout=5)
+        if response.status_code == 200:
+            data = response.json()
+            cards = data.get("data", [])
+            df = pd.DataFrame(cards)
+            if not df.empty:
+                # set-Spalte zerlegen
+                set_df = df["set"].apply(pd.Series)  # `set`-Objekt in separate Spalten zerlegen
+                set_columns = ["name", "series", "releaseDate"]  # Wähle gewünschte Felder
+                set_df = set_df[set_columns]  # Nur relevante Spalten behalten
+                set_df = set_df.rename(columns={'name': 'set_name'})
+                set_df = set_df.rename(columns={'releaseDate': 'release_date'})
+                # Füge die extrahierten Spalten dem Haupt-DataFrame hinzu
+                df = pd.concat([df.drop(columns=["set"]), set_df], axis=1)
+                # Konvertiere `releaseDate` zu einem Datumsformat
+                df["release_date"] = pd.to_datetime(df["release_date"], errors="coerce")
+                # Sortiere nach `releaseDate`
+                df = df.sort_values(by=["release_date", "id"], ascending=True)
+            return df
+        else:
+            print(f"Fehler: {response.status_code} - {response.text}")
+    except requests.exceptions.Timeout:
+        print("Timeout! Server has not responded in 5 Seconds.")
 
-    if response.status_code == 200:
-        data = response.json()
-        cards = data.get("data", [])
-        df = pd.DataFrame(cards)
-        if not df.empty:
-            # set-Spalte zerlegen
-            set_df = df["set"].apply(pd.Series)  # `set`-Objekt in separate Spalten zerlegen
-            set_columns = ["name", "series", "releaseDate"]  # Wähle gewünschte Felder
-            set_df = set_df[set_columns]  # Nur relevante Spalten behalten
-            set_df = set_df.rename(columns={'name': 'set_name'})
-            set_df = set_df.rename(columns={'releaseDate': 'release_date'})
-            # Füge die extrahierten Spalten dem Haupt-DataFrame hinzu
-            df = pd.concat([df.drop(columns=["set"]), set_df], axis=1)
 
-            # Konvertiere `releaseDate` zu einem Datumsformat
-            df["release_date"] = pd.to_datetime(df["release_date"], errors="coerce")
-
-            # Sortiere nach `releaseDate`
-            df = df.sort_values(by=["release_date", "id"], ascending=True)
-        return df
-    else:
-        print(f"Fehler: {response.status_code} - {response.text}")
-
-
-df = get_cards("arbok")
+df = get_cards(8)
 PokeList.print_list(df)
-PokeList.save_list(df, "arbok")
+#PokeList.save_list(df, "arbok")
 
